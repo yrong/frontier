eb771a30bb9272d4b8aedec590288dab8f0a958a
tgmichel <telmo@purestake.com>
2020-10-15 11:12:41 +0200

    Remove unnecessary FilteredParams

--- rpc/src/eth_pubsub.rs
+++ rpc/src/eth_pubsub.rs
@@ -19,7 +19,7 @@ use log::warn;
 use jsonrpc_pubsub::{typed::Subscriber, SubscriptionId, manager::SubscriptionManager};
 use frontier_rpc_core::EthPubSubApi::{self as EthPubSubApiT};
 use frontier_rpc_core::types::{
-	BlockNumber, Rich, Header, Bytes, Log, FilterAddress, Topic, VariadicValue,
+	Rich, Header, Bytes, Log, VariadicValue, Filter,
 	pubsub::{Kind, Params, Result as PubSubResult, PubSubSyncStatus}
 };
 use ethereum_types::{H256, U256};
@@ -33,7 +33,6 @@ use jsonrpc_core::{Result as JsonRpcResult, futures::{Future, Sink}};
 use frontier_rpc_primitives::{EthereumRuntimeRPCApi, TransactionStatus};
 
 use sc_network::{NetworkService, ExHashT};
-use crate::internal_err;
 
 pub struct EthPubSubApi<B: BlockT, P, C, BE, H: ExHashT> {
 	_pool: Arc<P>,
@@ -54,127 +53,19 @@ impl<B: BlockT, P, C, BE, H: ExHashT> EthPubSubApi<B, P, C, BE, H> {
 	}
 }
 
-impl<B: BlockT, P, C, BE, H: ExHashT> EthPubSubApi<B, P, C, BE, H> where
-	B: BlockT<Hash=H256> + Send + Sync + 'static,
-	P: TransactionPool<Block=B> + Send + Sync + 'static,
-	C: ProvideRuntimeApi<B> + StorageProvider<B,BE> +
-		BlockchainEvents<B> + AuxStore,
-	C: HeaderBackend<B> + HeaderMetadata<B, Error=BlockChainError> + 'static,
-	C::Api: EthereumRuntimeRPCApi<B>,
-	C: Send + Sync + 'static,
-	BE: Backend<B> + 'static,
-	BE::State: StateBackend<BlakeTwo256>,
-{
-	fn native_block_number(&self, number: Option<BlockNumber>) -> JsonRpcResult<Option<u32>> {
-		let mut native_number: Option<u32> = None;
-
-		if let Some(number) = number {
-			match number {
-				BlockNumber::Hash { hash, .. } => {
-					let id = self.load_hash(hash).unwrap_or(None);
-					if let Some(id) = id {
-						if let Ok(Some(block)) = self.client.runtime_api().current_block(&id) {
-							native_number = Some(block.header.number.as_u32());
-						}
-					}
-				},
-				BlockNumber::Num(_) => {
-					if let Some(number) = number.to_min_block_num() {
-						native_number = Some(number.unique_saturated_into());
-					}
-				},
-				BlockNumber::Latest => {
-					native_number = Some(
-						self.client.info().best_number.clone().unique_saturated_into() as u32
-					);
-				},
-				BlockNumber::Earliest => {
-					native_number = Some(0);
-				},
-				BlockNumber::Pending => {
-					native_number = None;
-				}
-			};
-		} else {
-			native_number = Some(
-				self.client.info().best_number.clone().unique_saturated_into() as u32
-			);
-		}
-		Ok(native_number)
-	}
-
-	fn filter_block(
-		&self,
-		params: Option<Params>
-	) -> (Option<u32>, Option<u32>) {
-		if let Some(Params::Logs(f)) = params {
-			let to_block: Option<u32> = if f.to_block.is_some() {
-				self.native_block_number(f.to_block).unwrap_or(None)
-			} else {
-				None
-			};
-			return (
-				self.native_block_number(f.from_block).unwrap_or(None),
-				to_block
-			);
-		}
-		(None, None)
-	}
-
-	// Asumes there is only one mapped canonical block in the AuxStore, otherwise something is wrong
-	fn load_hash(&self, hash: H256) -> JsonRpcResult<Option<BlockId<B>>> {
-		let hashes = match frontier_consensus::load_block_hash::<B, _>(self.client.as_ref(), hash)
-			.map_err(|err| internal_err(format!("fetch aux store failed: {:?}", err)))?
-		{
-			Some(hashes) => hashes,
-			None => return Ok(None),
-		};
-		let out: Vec<H256> = hashes.into_iter()
-			.filter_map(|h| {
-				if let Ok(Some(_)) = self.client.header(BlockId::Hash(h)) {
-					Some(h)
-				} else {
-					None
-				}
-			}).collect();
-
-		if out.len() == 1 {
-			return Ok(Some(
-				BlockId::Hash(out[0])
-			));
-		}
-		Ok(None)
-	}
-}
-
 struct FilteredParams {
-	from_block: Option<u32>,
-	to_block: Option<u32>,
-	block_hash: Option<H256>,
-	address: Option<FilterAddress>,
-	topics: Option<Topic>
+	filter: Option<Filter>,
 }
 
 impl FilteredParams {
 	pub fn new(
 		params: Option<Params>,
-		block_range: (Option<u32>, Option<u32>)
 	) -> Self {
 		if let Some(Params::Logs(d)) = params {
-			return FilteredParams {
-				from_block: block_range.0,
-				to_block: block_range.1,
-				block_hash: d.block_hash,
-				address: d.address,
-				topics: d.topics
-			};
+			return FilteredParams { filter: Some(d) };
 		}
 		FilteredParams {
-			from_block: None,
-			to_block: None,
-			block_hash: None,
-			address: None,
-			topics: None
+			filter: None
 		}
 	}
 
@@ -183,16 +74,17 @@ impl FilteredParams {
 		block: &ethereum::Block
 	) -> bool {
 		let mut out = true;
-		let number: u32 = UniqueSaturatedInto::<u32>::unique_saturated_into(
+		let number = UniqueSaturatedInto::<u64>::unique_saturated_into(
 			block.header.number
 		);
-		if let Some(from) = self.from_block {
-			if from > number {
+		let filter = self.filter.clone().unwrap();
+		if let Some(from) = filter.from_block {
+			if from.to_min_block_num().unwrap_or(0 as u64) > number {
 				out = false;
 			}
 		}
-		if let Some(to) = self.to_block {
-			if to < number {
+		if let Some(to) = filter.to_block {
+			if to.to_min_block_num().unwrap_or(0 as u64) < number {
 				out = false;
 			}
 		}
@@ -203,7 +95,7 @@ impl FilteredParams {
 		&self,
 		block_hash: H256
 	) -> bool {
-		if let Some(h) = self.block_hash {
+		if let Some(h) = self.filter.clone().unwrap().block_hash {
 			if h != block_hash { return false; }
 		}
 		true
@@ -213,7 +105,7 @@ impl FilteredParams {
 		&self,
 		log: &ethereum::Log
 	) -> bool {
-		if let Some(input_address) = &self.address {
+		if let Some(input_address) = &self.filter.clone().unwrap().address {
 			match input_address {
 				VariadicValue::Single(x) => {
 					if log.address != *x { return false; }
@@ -231,7 +123,7 @@ impl FilteredParams {
 		&self,
 		log: &ethereum::Log
 	) -> bool {
-		if let Some(input_topics) = &self.topics {
+		if let Some(input_topics) = &self.filter.clone().unwrap().topics {
 			match input_topics {
 				VariadicValue::Single(x) => {
 					if !log.topics.starts_with(&vec![*x]) {
@@ -349,10 +241,12 @@ impl SubscriptionResult {
 		block: &ethereum::Block,
 		params: &FilteredParams
 	) -> bool {
-		if !params.filter_block_range(block) ||
-			!params.filter_block_hash(block_hash) ||
-			!params.filter_address(log) || !params.filter_topics(log) {
-			return false;
+		if let Some(_) = params.filter {
+			if !params.filter_block_range(block) ||
+				!params.filter_block_hash(block_hash) ||
+				!params.filter_address(log) || !params.filter_topics(log) {
+				return false;
+			}
 		}
 		true
 	}
@@ -397,8 +291,7 @@ impl<B: BlockT, P, C, BE, H: ExHashT> EthPubSubApiT for EthPubSubApi<B, P, C, BE
 		kind: Kind,
 		params: Option<Params>,
 	) {
-		let filter_block = self.filter_block(params.clone());
-		let filtered_params = FilteredParams::new(params, filter_block);
+		let filtered_params = FilteredParams::new(params);
 		let client = self.client.clone();
 		let network = self.network.clone();
 		match kind {
