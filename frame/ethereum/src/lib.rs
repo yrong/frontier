e5815dcb91880b2ca084b0e939b7ff74d24ed693
tgmichel <telmo@purestake.com>
2020-10-22 19:03:54 +0200

    Rollback pallet-ethereum storage changes

--- frame/ethereum/src/lib.rs
+++ frame/ethereum/src/lib.rs
@@ -279,17 +279,9 @@ impl<T: Trait> Module<T> {
 		}
 
 		CurrentBlock::put(block.clone());
-		if receipts.len() > 0 {
-			CurrentReceipts::put(receipts.clone());
-		} else {
-			CurrentReceipts::kill();
-		}
+		CurrentReceipts::put(receipts.clone());
 
-		if statuses.len() > 0 {
-			CurrentTransactionStatuses::put(statuses.clone());
-		} else {
-			CurrentTransactionStatuses::kill();
-		}
+		CurrentTransactionStatuses::put(statuses.clone());
 
 		let digest = DigestItem::<T::Hash>::Consensus(
 			FRONTIER_ENGINE_ID,
